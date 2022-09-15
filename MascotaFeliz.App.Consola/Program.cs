a4ae84cc2f4f53248938af961cef6cb63bda4ea7
pmurciavilla <pmurciavilla@gmail.com>
2022-09-14 19:23:13 -0500

    Proyecto completo - Carpetas

---
+++ MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+using System.Collections.Generic;
+
+namespace MascotaFeliz.App.Consola
+{
+    class Program
+    {
+
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+        private static IRepositorioMascota _repoMascota = new RepositorioMascota(new Persistencia.AppContext());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //AddDueno();
+            //ListarDuenos();
+            //BuscarDueno(1);
+            //AddVeterinario();
+            //ListarVeterinarios();
+            //BuscarVeterinario(3);
+            //AddMascota();
+            //ListarMascotas();
+            //BuscarMascota(1);
+
+        }
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                //Cedula = "1212",
+                Nombres = "Juan",
+                Apellidos = "Sin Miedo",
+                Direccion = "Bajo un puente",
+                Telefono = "1234567891",
+                Correo = "juansinmiedo@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
+        }
+        private static void AddVeterinario()
+        {
+            var veterinario = new Veterinario
+            {
+                //Cedula = "1212",
+                Nombres = "Pedro",
+                Apellidos = "Perez",
+                Direccion = "XXX",
+                Telefono = "1234567891",
+                TarjetaProfesional = "5464748"
+            };
+            _repoVeterinario.AddVeterinario(veterinario);
+        }
+        
+        private static void AddMascota()
+        {
+            var mascota = new Mascota
+            {
+                //Cedula = "1212",
+                Nombre = "Lucas",
+                Color = "Cafe",
+                Especie = "Perro",
+                Raza = "Bulldog"
+            };
+            _repoMascota.AddMascota(mascota);
+        }
+        private static void ListarDuenos()
+        {
+            var duenos = _repoDueno.GetAllDuenos();
+            foreach (Dueno d in duenos)
+            {
+                Console.WriteLine(d.Nombres + " " + d.Apellidos);
+            }
+        }
+        private static void BuscarDueno(int idDueno)
+        {
+            var dueno = _repoDueno.GetDueno(idDueno);
+            Console.WriteLine(dueno.Id + " " + dueno.Nombres + " " + dueno.Apellidos + " " + dueno.Direccion + " " + dueno.Telefono + " " + dueno.Correo);
+        }
+        private static void ListarVeterinarios()
+        {
+            var veterinarios = _repoVeterinario.GetAllVeterinarios();
+            foreach (Veterinario d in veterinarios)
+            {
+                Console.WriteLine(d.Nombres + " " + d.Apellidos);
+            }
+        }
+        private static void BuscarVeterinario(int idVeterinario)
+        {
+            var veterinario = _repoVeterinario.GetVeterinario(idVeterinario);
+            Console.WriteLine(veterinario.Id + " " + veterinario.Nombres + " " + veterinario.Apellidos + " " + veterinario.Direccion + " " + veterinario.Telefono + " " + veterinario.TarjetaProfesional);
+        }
+        private static void ListarMascotas()
+        {
+            var mascotas = _repoMascota.GetAllMascotas();
+            foreach (Mascota d in mascotas)
+            {
+                Console.WriteLine(d.Nombre + " " + d.Especie);
+            }
+        }
+        private static void BuscarMascota(int idMascota)
+        {
+            var mascota = _repoMascota.GetMascota(idMascota);
+            Console.WriteLine(mascota.Id + " " + mascota.Nombre + " " + mascota.Color + " " + mascota.Especie + " " + mascota.Raza + " " + mascota.Dueno+ " " + mascota.Historia);
+        }
+    }
+}
