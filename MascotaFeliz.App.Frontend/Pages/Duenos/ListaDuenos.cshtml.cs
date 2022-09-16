fcb808e6115645021eccd64a45f3f8e4689234c8
Paola Murcia <pmurciavilla@gmail.com>
2022-09-16 17:17:22 -0500

    actualizando

--- MascotaFeliz.App.Frontend/Pages/Duenos/ListaDuenos.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Duenos/ListaDuenos.cshtml.cs
@@ -6,15 +6,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MascotaFeliz.App.Dominio;
 using MascotaFeliz.App.Persistencia;
-//CambiosPrueba
+
 namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListaDuenosModel : PageModel
     {
-        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
-        public IEnumerable<Dueno> listaDuenos{get;set;}
+        private readonly IRepositorioDueno _repoDueno;
+        public IEnumerable<Dueno> listaDuenos {get;set;}
+        public ListaDuenosModel()
+        {
+            this._repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        }
+
         public void OnGet()
         {
+            listaDuenos = _repoDueno.GetAllDuenos();
         }
     }
 }
